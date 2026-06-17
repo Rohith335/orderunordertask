f9bb4f7f430b6a21738b1c22ae885b58985338bf
Rohith335 <rohithvengaladasu@gmail.com>
2026-06-17 13:37:15 +0530

    Add files via upload

---
+++ listdatatype.py
@@ -0,0 +1,9 @@
+cities = ["hyderabad", "warangal", "khammam", "karimnagar", "siddipet"]
+print(cities[2])
+print(cities[0:3])
+cities.sort()
+cities.append("sangareddy")
+cities.remove("hyderabad")
+print(len(cities))
+result= cities
+print(result)
